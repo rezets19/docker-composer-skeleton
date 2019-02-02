4555dd544a8650d945df9ebe698dcd62d53efd74
ruslan <null>
2019-02-02 20:39:38 +0200

    win paths

--- docker/scripts/cmd/docker-start.cmd
+++ docker/scripts/cmd/docker-start.cmd
@@ -7,7 +7,7 @@ SET cwd=%~dp0
 :: Go to Docker for Windows settings > Shared Drives > Reset credentials > select drive > Apply;
 SET COMPOSE_CONVERT_WINDOWS_PATHS=1
 
-docker-compose -f %cwd%\..\docker-compose.yml -p %containers_prefix% build
-docker-compose -f %cwd%\..\docker-compose.yml -p %containers_prefix% up -d
+docker-compose -f %cwd%..\..\docker-compose.yml -p %containers_prefix% build
+docker-compose -f %cwd%..\..\docker-compose.yml -p %containers_prefix% up -d
 
 docker exec -it app-db bash -c "/schema/setup.sh"
